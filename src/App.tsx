37c7be2803f2c46b7c1b06891495e76d627c2b0b
ZB <zachary_chan+cs1300@brown.edu>
2022-12-08 17:06:41 -0500

    fire

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Text } from '@nextui-org/react'
 
 function App() {
   return (
+
+
     <div className="App">
+      <div className='gridWrapper'>
+        <div className='title'>
+          <Text className='titleText'>CS1300 Final Portfolio</Text>
+        </div>
+        <a href='#development' className='tocItem'>
+          <video width="100%" height="100%" autoPlay muted loop>
+            <source src={require('./croppedDevelopment.mp4')} type="video/mp4"></source>
+          </video>
+        </a>
+        <a href='#iterative' className='tocItem'>
+          <video width="100%" height="100%" autoPlay muted loop>
+            <source src={require('./croppedPivo.mp4')} type="video/mp4"></source>
+          </video>
+        </a>
+        <a href='#redesign' className='tocItem'>
+          <video width="100%" height="100%" autoPlay muted loop>
+            <source src={require('./croppedRedesign.mp4')} type="video/mp4"></source>
+          </video>
+        </a>
+        <div className='tocItem'>1</div>
+      </div>
+
+      <div className='page' id='development'> development </div>
+
+      <div className='page' id='iterative'>iterative design</div>
+
+      <div className='page' id='redesign'>redesign</div>
+      
+      <div className='page'></div>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
