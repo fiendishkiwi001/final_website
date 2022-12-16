452d80fb9a7523cab32e5f6910d5afa63c089d33
ZB <zachary_chan+cs1300@brown.edu>
2022-12-16 12:21:51 -0500

    about me

--- src/App.tsx
+++ src/App.tsx
@@ -35,16 +35,47 @@ function App() {
         </a>
       </div>
 
-      <div className='page' id='development'>
-        <Text>Development</Text>
+      <div className='page'>
+        <div className='aboutMe'>
+          <div className='kiwi'>
+            <img src={require('./kiwi.PNG')}></img>
+          </div>
+          <div className='biography'>
+            <div className='hi'>
+            <Text size="$7xl">Hi, I'm&nbsp;</Text><Text size="$7xl" color='#8EE53F'>________________</Text>
+            </div>
+            <div className='bullets'>
+              <div className='hi'>
+                <Text size="$4xl">•&nbsp;&nbsp;</Text><Text size="$4xl" color='#8EE53F'>fiendishkiwi001</Text>
+              </div>
+              <div className='hi'>
+                <Text size="$4xl">•&nbsp;&nbsp;</Text><Text size="$4xl" color='#8EE53F'>a Brown computer science student</Text>
+              </div>
+              <div className='hi'>
+                <Text size="$4xl">•&nbsp;&nbsp;</Text><Text size="$4xl" color='#8EE53F'>passionate about developing web applications with practical uses</Text>
+              </div>
+              <div className='hi'>
+                <Text size="$4xl">•&nbsp;&nbsp;</Text><Text size="$4xl" color='#8EE53F'>always learning and applying new design concepts</Text>
+              </div>
+              <div className='hi'>
+                <Text size="$4xl">•&nbsp;&nbsp;</Text><Text size="$4xl" color='#8EE53F'>excited to present my CS1300: UIUX journey!</Text>
+              </div>
+            </div>
+          </div>
+        </div>
+        
       </div>
+
+      <div className='page' id='personas'>personas</div>
+
+      <div className='page' id='redesign'>redesign</div>
 
       <div className='page' id='iterative'>iterative design</div>
 
-      <div className='page' id='redesign'>redesign</div>
+      <div className='page' id='development'>
+        <Text>Development</Text>
+      </div>
       
-      <div className='page' id='personas'>personas</div>
-
     </div>
   );
 }
