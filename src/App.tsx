7fa7cbc83d8e064a875520b75e4cee2de9af8423
ZB <zachary_chan+cs1300@brown.edu>
2022-12-16 13:46:42 -0500

    skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -8,33 +8,6 @@ function App() {
 
 
     <div className="App">
-      <div className='gridWrapper'>
-        <div className='title'>
-          <Text className='titleText'>CS1300 Final Portfolio</Text>
-          <Text>Hover over project links to preview!</Text>
-        </div>
-        <a href='#development' className='tocItem'>
-          <video width="100%" height="100%" autoPlay muted loop>
-            <source src={require('./croppedDevelopment.mp4')} type="video/mp4"></source>
-          </video>
-        </a>
-        <a href='#iterative' className='tocItem'>
-          <video width="100%" height="100%" autoPlay muted loop>
-            <source src={require('./croppedPivo.mp4')} type="video/mp4"></source>
-          </video>
-        </a>
-        <a href='#redesign' className='tocItem'>
-          <video width="100%" height="100%" autoPlay muted loop>
-            <source src={require('./croppedRedesign.mp4')} type="video/mp4"></source>
-          </video>
-        </a>
-        <a href='#personas' className='tocItem'>
-          <video width="100%" height="100%" autoPlay muted loop>
-            <source src={require('./croppedPersonas.mp4')} type="video/mp4"></source>
-          </video>
-        </a>
-      </div>
-
       <div className='page'>
         <div className='aboutMe'>
           <div className='kiwi'>
@@ -42,7 +15,7 @@ function App() {
           </div>
           <div className='biography'>
             <div className='hi'>
-            <Text size="$7xl">Hi, I'm&nbsp;</Text><Text size="$7xl" color='#8EE53F'>________________</Text>
+            <Text size="$6xl">Hi, I'm&nbsp;</Text><Text size="$6xl" color='#8EE53F'>________________</Text>
             </div>
             <div className='bullets'>
               <div className='hi'>
@@ -63,19 +36,160 @@ function App() {
             </div>
           </div>
         </div>
-        
       </div>
 
-      <div className='page' id='personas'>personas</div>
+      <div className='page'>
+        <Text css={{margin: 0}} className='tocTitle' size='$lg' color='#8EE53F'>Navigate site using project preview links below</Text>
+        <div className='gridWrapper'>
 
-      <div className='page' id='redesign'>redesign</div>
-
-      <div className='page' id='iterative'>iterative design</div>
-
-      <div className='page' id='development'>
-        <Text>Development</Text>
+          <a href='#personas' className='tocItem'>
+            <video width="100%" height="100%" autoPlay muted loop>
+              <source src={require('./croppedPersonas.mp4')} type="video/mp4"></source>
+            </video>
+          </a>
+          <a href='#redesign' className='tocItem'>
+            <video width="100%" height="100%" autoPlay muted loop>
+              <source src={require('./croppedRedesign.mp4')} type="video/mp4"></source>
+            </video>
+          </a>
+          <a href='#iterative' className='tocItem'>
+            <video width="100%" height="100%" autoPlay muted loop>
+              <source src={require('./croppedPivo.mp4')} type="video/mp4"></source>
+            </video>
+          </a>
+          
+          <a href='#development' className='tocItem'>
+            <video width="100%" height="100%" autoPlay muted loop>
+              <source src={require('./croppedDevelopment.mp4')} type="video/mp4"></source>
+            </video>
+          </a>
+        </div>
       </div>
       
+
+      <div className='projectPage' id='personas'>
+        <div className='projectTitle'>
+          <Text size="$6xl">Personas & Storyboarding</Text>
+        </div>
+        <div className='projectContent'>
+          <div className='halfPage'>
+            <div>
+              <img className='personaImages' src={require('./remoteSS.png')}></img>
+              <img className='personaImages' src={require('./empathyMap.png')}></img>
+            </div>
+          </div>
+          <div className='halfPage'>
+            <Text size="$4xl" color='#8EE53F' css={{margin: 0}}>Objectives</Text>
+
+            <div className='projectBullets'>
+              <div className='projectBulletItem'>
+                <Text size="$lg" color='#8EE53F' css={{margin: 0}}>•&nbsp;&nbsp;</Text>
+                <Text size="$lg" css={{margin: 0}}>
+                  Create a new interface for any public interface (a Roku TV remote in this case)
+                </Text>
+              </div>
+              <div className='projectBulletItem'>
+                <Text size="$lg" color='#8EE53F' css={{margin: 0}}>•&nbsp;&nbsp;</Text>
+                <Text size="$lg" css={{margin: 0}}>
+                  Interview potential users to construct corresponding personas and empathy maps
+                </Text>
+              </div>
+              <div className='projectBulletItem'>
+                <Text size="$lg" color='#8EE53F' css={{margin: 0}}>•&nbsp;&nbsp;</Text>
+                <Text size="$lg" css={{margin: 0}}>
+                  Consider these personas and create a storyboard for the chosen product in order to better understand the complete user experience
+                </Text>
+              </div>
+            </div>
+
+            <Text size="$4xl" color='#8EE53F' css={{margin: 0}}>Decisions</Text>
+
+            <div className='projectBullets'>
+              <div className='projectBulletItem'>
+                <Text size="$lg" color='#8EE53F' css={{margin: 0}}>•&nbsp;&nbsp;</Text>
+                <Text size="$lg" css={{margin: 0}}>
+                  Create a new interface for any public interface (a Roku TV remote in this case)
+                </Text>
+              </div>
+              <div className='projectBulletItem'>
+                <Text size="$lg" color='#8EE53F' css={{margin: 0}}>•&nbsp;&nbsp;</Text>
+                <Text size="$lg" css={{margin: 0}}>
+                  Interview potential users to construct corresponding personas and empathy maps.
+                </Text>
+              </div>
+              <div className='projectBulletItem'>
+                <Text size="$lg" color='#8EE53F' css={{margin: 0}}>•&nbsp;&nbsp;</Text>
+                <Text size="$lg" css={{margin: 0}}>
+                  Consider these personas and create a storyboard for the chosen product in order to better understand the complete user experience.
+                </Text>
+              </div>
+            </div>
+
+            <Text size="$4xl" color='#8EE53F' css={{margin: 0}}>Reflections</Text>
+            <Text size="$4xl" color='#8EE53F' css={{margin: 0}}>Link to Page</Text>
+            <Text size="$lg">
+              <a href='https://fiendishkiwi001.github.io/storyboard/' target="_blank" rel="noreferrer">fiendishkiwi001.github.io/storyboard</a>
+            </Text>
+          </div>
+        </div>
+      </div>
+
+      <div className='projectPage' id='redesign'>
+        <div className='projectTitle'>
+          <Text size="$6xl">Responsive Redesign</Text>
+        </div>
+        <div className='projectContent'>
+          <div className='halfPage'>
+            <Text size="$4xl" color='#8EE53F' css={{margin: 0}}>Objectives</Text>
+            <Text size="$4xl" color='#8EE53F' css={{margin: 0}}>Decisions</Text>
+            <Text size="$4xl" color='#8EE53F' css={{margin: 0}}>Reflections</Text>
+            <Text size="$4xl" color='#8EE53F' css={{margin: 0}}>Link to Page</Text>
+            <Text size="$lg">Heroku deployment unavailable :(</Text>
+          </div>
+          <div className='halfPage'>
+            
+          </div>
+        </div>
+      </div>
+
+      <div className='projectPage' id='iterative'>
+        <div className='projectTitle'>
+          <Text size="$6xl">Iterative Design</Text>
+        </div>
+        <div className='projectContent'>
+          <div className='halfPage'>
+            <Text size="$4xl" color='#8EE53F' css={{margin: 0}}>Objectives</Text>
+            <Text size="$4xl" color='#8EE53F' css={{margin: 0}}>Decisions</Text>
+            <Text size="$4xl" color='#8EE53F' css={{margin: 0}}>Reflections</Text>
+            <Text size="$4xl" color='#8EE53F' css={{margin: 0}}>Link to Page</Text>
+            <Text size="$lg">Figma is not anonymous :(</Text>
+          </div>
+          <div className='halfPage'>
+            
+          </div>
+        </div>
+      </div>
+
+      <div className='projectPage' id='development'>
+        <div className='projectTitle'>
+          <Text size="$6xl">Development</Text>
+        </div>
+        <div className='projectContent'>
+          <div className='halfPage'>
+            
+          </div>
+          <div className='halfPage'>
+            <Text size="$4xl" color='#8EE53F' css={{margin: 0}}>Objectives</Text>
+            <Text size="$4xl" color='#8EE53F' css={{margin: 0}}>Decisions</Text>
+            <Text size="$4xl" color='#8EE53F' css={{margin: 0}}>Reflections</Text>
+            <Text size="$4xl" color='#8EE53F' css={{margin: 0}}>Link to Page</Text>
+            <Text size="$lg">
+              <a href='https://fiendishkiwi001.github.io/development/' target="_blank" rel="noreferrer">fiendishkiwi001.github.io/development</a>
+            </Text>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
